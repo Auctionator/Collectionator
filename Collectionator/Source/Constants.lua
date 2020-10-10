72c3470478da2c2868088aaa0b8967b2b8df853e
plusmouse <plusmouse@protonmail.com>
2020-10-10 17:50:07 +0100

    Simplify tmog scanner

--- Collectionator/Source/Constants.lua
+++ Collectionator/Source/Constants.lua
@@ -1,5 +1,5 @@
 Collectionator.Constants = {
   PET_SCAN_STEP_SIZE = 2000,
   TOY_SCAN_STEP_SIZE = 1000,
-  TMOG_SCAN_STEP_SIZE = 500,
+  TMOG_SCAN_STEP_SIZE = 900,
 }
--- Collectionator/Source/Scanner/TMog.lua
+++ Collectionator/Source/Scanner/TMog.lua
@@ -1,41 +1,8 @@
-local INVENTORY_TYPES_TO_SLOT = {
-  ["INVTYPE_AMMO"] = {0},
-  ["INVTYPE_HEAD"] = {1},
-  ["INVTYPE_NECK"] = {2},
-  ["INVTYPE_SHOULDER"] = {3},
-  ["INVTYPE_BODY"] = {4},
-  ["INVTYPE_CHEST"] = {5},
-  ["INVTYPE_ROBE"] = {5},
-  ["INVTYPE_WAIST"] = {6},
-  ["INVTYPE_LEGS"] = {7},
-  ["INVTYPE_FEET"] = {8},
-  ["INVTYPE_WRIST"] = {9},
-  ["INVTYPE_HAND"] = {10},
-  --["INVTYPE_FINGER"] = {11,12},
-  --["INVTYPE_TRINKET"] = {13,14},
-  ["INVTYPE_CLOAK"] = {15},
-  ["INVTYPE_WEAPON"] = {16,17},
-  ["INVTYPE_SHIELD"] = {17},
-  ["INVTYPE_2HWEAPON"] = {16},
-  ["INVTYPE_WEAPONMAINHAND"] = {16},
-  ["INVTYPE_WEAPONOFFHAND"] = {17},
-  ["INVTYPE_HOLDABLE"] = {17},
-  ["INVTYPE_RANGED"] = {18},
-  ["INVTYPE_THROWN"] = {18},
-  ["INVTYPE_RANGEDRIGHT"] = {18},
-  ["INVTYPE_RELIC"] = {18},
-  ["INVTYPE_TABARD"] = {19},
-}
-
 CollectionatorTMogScannerFrameMixin = {}
 
 function CollectionatorTMogScannerFrameMixin:OnLoad()
   self.sources = {}
-  self.droppedCount = 0
   self.leftCount = 0
-
-  self.mode = "player"
-  self:ClearScene()
 
   self.dirty = false
 
@@ -78,74 +45,32 @@ function CollectionatorTMogScannerFrameMixin:Process()
   )
 
   self.sources = {}
-  self.droppedCount = 0
   self.leftCount = #self.fullScan
 
-  assert(self:IsReady())
   self:BatchStep(1, Collectionator.Constants.TMOG_SCAN_STEP_SIZE)
 end
 
-function CollectionatorTMogScannerFrameMixin:ClearScene()
-  self.ModelScene:ClearScene();
-  self.ModelScene:SetViewInsets(0, 0, 0, 0);
-  self.ModelScene:TransitionToModelSceneID(290, CAMERA_TRANSITION_TYPE_IMMEDIATE, CAMERA_MODIFICATION_TYPE_DISCARD, true);
-  self:ResetPlayer()
-end
+function CollectionatorTMogScannerFrameMixin:GetItemSource(index, link)
+  local _, source = C_TransmogCollection.GetItemInfo(link)
 
-function CollectionatorTMogScannerFrameMixin:ResetPlayer()
-  SetupPlayerForModelScene(self.ModelScene, nil, false, false);
-end
-
-function CollectionatorTMogScannerFrameMixin:PlayerActor()
-  return self.ModelScene:GetPlayerActor()
-end
-
-function CollectionatorTMogScannerFrameMixin:IsReady()
-  return self:PlayerActor() and self:PlayerActor():IsLoaded()
-end
-
-function CollectionatorTMogScannerFrameMixin:OnUpdate()
-  if not self:PlayerActor():IsLoaded() then
-    self:ClearScene()
-  end
-end
-
-
-function CollectionatorTMogScannerFrameMixin:GetSlotSource(index, link)
-  local possibleSlots = INVENTORY_TYPES_TO_SLOT[select(9, GetItemInfo(link))]
-  if not possibleSlots then
-    self.droppedCount = self.droppedCount + 1
-    return
-  elseif #possibleSlots > 1 then
-    self:ResetPlayer()
-  end
-
-  local pa = self.ModelScene:GetPlayerActor()
-
-  pa:TryOn(link)
-
-  for _, slot in ipairs(possibleSlots) do
-    local source = pa:GetSlotTransmogSources(slot)
-    if source ~= 0 then
-      local visual = C_TransmogCollection.GetSourceInfo(source).visualID
-      local set = C_TransmogCollection.GetAllAppearanceSources(visual)
-      if #set == 0 then
-        set = {source}
-      end
-      table.insert(self.sources, {
-        id = source, visual = visual, index = index, set = set,
-        levelRequired = select(5, GetItemInfo(link)),
-      })
-      return
+  if source ~= nil then
+    local visual = C_TransmogCollection.GetSourceInfo(source).visualID
+    local set = C_TransmogCollection.GetAllAppearanceSources(visual)
+    if #set == 0 then
+      set = {source}
     end
+    table.insert(self.sources, {
+      id = source, visual = visual, index = index, set = set,
+      levelRequired = select(5, GetItemInfo(link)),
+    })
+    return
   end
-  self.droppedCount = self.droppedCount + 1
 end
 
 function CollectionatorTMogScannerFrameMixin:BatchStep(start, limit)
   Auctionator.Debug.Message("CollectionatorTMogScannerFrameMixin:BatchStep", start, limit)
   if start > #self.fullScan then
-    Auctionator.Debug.Message("CollectionatorTMogScannerFrameMixin:BatchStep", "READY", start, self.droppedCount, #self.sources)
+    Auctionator.Debug.Message("CollectionatorTMogScannerFrameMixin:BatchStep", "READY", start, #self.sources)
     return
   end
 
@@ -155,9 +80,7 @@ function CollectionatorTMogScannerFrameMixin:BatchStep(start, limit)
     local item = Item:CreateFromItemID(self.fullScan[i].replicateInfo[17])
     item:ContinueOnItemLoad((function(index, link)
       return function()
-        if IsDressableItem(link) then
-          self:GetSlotSource(index, link)
-        end
+        self:GetItemSource(index, link)
 
         self.leftCount = self.leftCount - 1
         if self.leftCount == 0 then
