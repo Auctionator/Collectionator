264b7076600e2a37a0a44bf48ac3450e07805c96
plusmouse <plusmouse@protonmail.com>
2021-02-13 19:08:06 +0000

    cleanup 2

--- Collectionator/Source/ResultsListing/Row.lua
+++ Collectionator/Source/ResultsListing/Row.lua
@@ -40,3 +40,62 @@ function CollectionatorRowMixin:OnEvent(eventName, itemKey)
     :Fire(self, Collectionator.Events.ShowBuyoutOptions, self:GetSearchResult(itemKey), self.rowData)
     :UnregisterSource(self)
 end
+
+local function GetIdenticalLinkItem(itemLink, itemKey)
+  for index = 1, C_AuctionHouse.GetNumItemSearchResults(itemKey) do
+    local info = C_AuctionHouse.GetItemSearchResultInfo(itemKey, index)
+    if info.itemLink == itemLink then
+      return info
+    end
+  end
+
+  return nil
+end
+
+CollectionatorTMogRowMixin = CreateFromMixins(CollectionatorRowMixin)
+
+function CollectionatorTMogRowMixin:StartSearch()
+  local itemID = GetItemInfoInstant(self.rowData.itemLink)
+  local itemKey = {itemID = itemID, itemLevel = 0, itemSuffix = 0, battlePetSpeciesID = 0}
+
+  Auctionator.AH.SendSellSearchQuery(itemKey, Collectionator.Constants.ITEM_SORTS, true)
+end
+
+function CollectionatorTMogRowMixin:GetSearchResult(itemKey)
+  return GetIdenticalLinkItem(self.rowData.itemLink, itemKey)
+end
+
+CollectionatorPetRowMixin = CreateFromMixins(CollectionatorRowMixin)
+
+function CollectionatorPetRowMixin:StartSearch()
+  -- Use that an Auctionator database key for a pet has the format p:[speciesID]
+  local _, petID = strsplit(":", Auctionator.Utilities.ItemKeyFromLink(self.rowData.itemLink))
+  local itemKey = C_AuctionHouse.MakeItemKey(Auctionator.Constants.PET_CAGE_ID, 0, 0, tonumber(petID))
+
+  Auctionator.AH.SendSearchQuery(itemKey, Collectionator.Constants.ITEM_SORTS, true)
+end
+
+function CollectionatorPetRowMixin:GetSearchResult(itemKey)
+  return GetIdenticalLinkItem(self.rowData.itemLink, itemKey)
+end
+
+CollectionatorToyMountRowMixin = CreateFromMixins(CollectionatorRowMixin)
+
+function CollectionatorToyMountRowMixin:StartSearch()
+  local itemID = GetItemInfoInstant(self.rowData.itemLink)
+  local itemKey = C_AuctionHouse.MakeItemKey(itemID)
+
+  self.expectedItemID = itemID
+  Auctionator.AH.SendSearchQuery(itemKey, Collectionator.Constants.ITEM_SORTS, true)
+end
+
+function CollectionatorToyMountRowMixin:GetSearchResult(itemKey)
+  for index = 1, C_AuctionHouse.GetNumItemSearchResults(itemKey) do
+    local info = C_AuctionHouse.GetItemSearchResultInfo(itemKey, index)
+    if GetItemInfoInstant(info.itemLink) == self.expectedItemID then
+      return info
+    end
+  end
+
+  return nil
+end
