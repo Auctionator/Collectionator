3c0c9ae5e6bb62e57fa25aaa131b00bbc8a128b4
plusmouse <plusmouse@protonmail.com>
2022-08-19 18:00:04 +0100

    Remove nested Collectionator directory

--- GetRecipesToSpellIDs/do-all.sh
+++ GetRecipesToSpellIDs/do-all.sh
@@ -3,6 +3,6 @@ mkdir -p tmp
 cd tmp
 
 ../GetRecipesToSpellIDs/get-files.sh
-../GetRecipesToSpellIDs/convert-recipes-to-spell-ids.py >../Collectionator/Data/RecipesToSpellIDs.lua
+../GetRecipesToSpellIDs/convert-recipes-to-spell-ids.py >../Data/RecipesToSpellIDs.lua
 
 cd ../
