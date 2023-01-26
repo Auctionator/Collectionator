11911be7e6f921695548d1a673416e45e4f39b8a
plusmouse <plusmouse@protonmail.com>
2023-01-26 11:09:02 +0000

    TMog: Enable poor/common gear qualities by default

--- Source/Tab/TMogView.lua
+++
@@ -1,8 +0,0 @@
-CollectionatorTMogViewMixin = CreateFromMixins(CollectionatorViewMixin)
-
-function CollectionatorTMogViewMixin:OnLoad()
-  CollectionatorViewMixin.OnLoad(self)
-
-  self.QualityFilter:ToggleFilter(Enum.ItemQuality.Poor)
-  self.QualityFilter:ToggleFilter(Enum.ItemQuality.Common)
-end
