08b141a9e09176383f2b1ff910ca2a0f27f54bab
plusmouse <plusmouse@protonmail.com>
2023-03-25 12:53:57 +0000

    Fix quantities in summary scan mode

--- Source/Utilities/ExtractWantedItems.lua
+++ Source/Utilities/ExtractWantedItems.lua
@@ -48,18 +48,42 @@ function Collectionator.Utilities.ReplicateExtractWantedItems(grouped, fullScan)
   return result
 end
 
+local function AddQuantities(array, fullScan)
+  local total = 0
+  for _, r in ipairs(array) do
+    total = total + fullScan[r.index].totalQuantity
+  end
+  return total
+end
+local function SummaryCombineForCheapest(array, getName, getPrice, fullScan)
+  SortByPriceAscending(array, getPrice)
+
+  array[1].quantity = AddQuantities(array, fullScan)
+  array[1].allNames = {}
+  for index, item in ipairs(array) do
+    local name = getName(item)
+    if tIndexOf(array[1].allNames, name) == nil then
+      table.insert(array[1].allNames, name)
+    end
+  end
+
+
+  return array[1]
+end
+
 function Collectionator.Utilities.SummaryExtractWantedItems(grouped, fullScan)
   local result = {}
 
   for _, array in pairs(grouped) do
     table.insert(result,
-      CombineForCheapest(array,
+      SummaryCombineForCheapest(array,
         function(item)
           return item.itemKeyInfo.itemName
         end,
         function(item)
           return fullScan[item.index].minPrice
-        end
+        end,
+        fullScan
       )
     )
   end
