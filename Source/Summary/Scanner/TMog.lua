99334fcd86067dbcfd778bd3f6a2ea922ef1f4c5
plusmouse <plusmouse@protonmail.com>
2023-03-28 07:18:46 +0100

    Summary: Restore usable item level feature for tmogs

--- Source/Summary/Scanner/TMog.lua
+++ Source/Summary/Scanner/TMog.lua
@@ -48,6 +48,7 @@ function CollectionatorSummaryTMogScannerFrameMixin:GetItem(index, itemKeyInfo, 
     return {
       id = source, visual = visual, index = index, set = set,
       armor = armorType, weapon = weaponType, slot = inventorySlot,
+      levelRequired = C_AuctionHouse.GetItemKeyRequiredLevel(scanInfo.itemKey),
     }
 
   else
